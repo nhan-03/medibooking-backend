19c2415a2937cecf49363055892dd60793b7abc4
HuuNhan <nhan0925195109@gmail.com>
2025-07-25 08:24:23 +0700

    Initial commit with .gitignore

---
+++ src/main/java/com/medibooking/backend/MedibookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.medibooking.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedibookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedibookingBackendApplication.class, args);
+	}
+
+}
